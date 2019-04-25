997069da3c4c0f18f51b64576c86f22f5b6ccc60
Prima Aulia Gusta <primaulia@gmail.com>
2019-04-25 16:21:43 +0800

    setup for livecode

---
+++ interface.rb
@@ -0,0 +1,10 @@
+# TASK 1
+
+# FETCH IMDB TOP MOVIES URL
+# - Scrape IMDB top movies url www.imdb.com/chart/top
+
+# Method name = #scrape_url
+# - PARAMS = nil
+# - RETURN = Array of urls strings
+
+
---
+++ scraper.rb
@@ -0,0 +1,38 @@
+# TASK 1
+
+# FETCH IMDB TOP MOVIES URL
+# - Scrape IMDB top movies url www.imdb.com/chart/top
+
+# Method name = #scrape_url
+# - PARAMS = nil
+# - RETURN = Array of urls strings
+
+
+# TASK 2
+
+# FETCH IMDB TOP MOVIES URL
+# - Scrape individual movie url e.g. http://www.imdb.com/title/tt0111161
+
+# Method name = #scrape_movie
+# - PARAMS = String of individual movie url
+# - RETURN = Hash of movie details
+# e.g.
+
+# {
+#   cast: [ "Christian Bale", "Heath Ledger", "Aaron Eckhart" ],
+#   director: "Christopher Nolan",
+#   storyline: "When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+#   title: "The Dark Knight",
+#   year: 2008
+# }
+
+
+# TASK 3
+
+# STORE HASH INTO THE YAML FILE
+
+# Method name = #store_to_yaml
+# - PARAMS = ARRAY of HASH
+# - RETURN = nil (updated .yml file)
+
+
